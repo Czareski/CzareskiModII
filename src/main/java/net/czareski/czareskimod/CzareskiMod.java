a7fbd2911d4c99487f148fa434ccbea7c2322243
Czareski <kubaucher202@gmail.com>
2022-06-12 02:10:55 +0200

    12.06.2022, 2:10: dodany pierwszy item

--- src/main/java/net/czareski/czareskimod/CzareskiMod.java
+++ src/main/java/net/czareski/czareskimod/CzareskiMod.java
@@ -1,5 +1,6 @@
 package net.czareski.czareskimod;
 
+import net.czareski.czareskimod.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,14 +9,14 @@ public class CzareskiMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("czareskimod");
+	public static final String MOD_ID = "czareskimod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.RegisterModItems();
 	}
 }
---
+++ src/main/java/net/czareski/czareskimod/items/ModItems.java
@@ -0,0 +1,22 @@
+package net.czareski.czareskimod.items;
+
+import net.czareski.czareskimod.CzareskiMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+
+    private static final Item STICK_OF_POWER = registerItem("stick_of_power",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(CzareskiMod.MOD_ID, name), item);
+    }
+    public static void RegisterModItems() {
+        CzareskiMod.LOGGER.info("Registering mod item for" + CzareskiMod.MOD_ID);
+    }
+}
